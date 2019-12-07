898d9c8552f14b130810926895cf74f778cc3d64
Yaroslav Opanasenko <yaroslav.opanasenko99@gmail.com>
2019-12-07 16:02:26 +0100

    added tools and audio res

---
+++ Assets/Tools/AudioCache.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCache
+{
+    private readonly string PATH = "Audio/";
+
+    private AudioClip[] clips;
+    private Dictionary<string, AudioClip> links = new Dictionary<string, AudioClip>();
+
+    public AudioCache Load()
+    {
+        clips = Resources.LoadAll<AudioClip>(PATH);
+        foreach (AudioClip clip in clips)
+        {
+            links.Add(clip.name, clip);
+        }
+        return this;
+    }
+
+    public AudioClip Get(string name)
+    {
+        return links[name];
+    }
+}
+
