a9127f78f4e08903a9e7c43f10acc45c9a5c70f5
Yaroslav Opanasenko <yaroslav.opanasenko99@gmail.com>
2019-12-16 13:32:03 +0100

    implemented mob combat + fix

---
+++ Assets/Game/World/Actor/ActorBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ActorBehaviour<A> : MonoBehaviour where A : Actor
+{
+    private A actor;
+
+    public ActorBehaviour<A> Assign(A actor)
+    {
+        this.actor = actor;
+        return this;
+    }
+
+    public A Actor()
+    {
+        return actor;
+    }
+}
---
+++ Assets/Game/World/Actor/Mob/MobCombat.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobCombat : ActorBehaviour<Mob>
+{
+    private float speed = 0.2f;
+    private int damage = 5; 
+
+    private float timer;
+    private Player victim;
+
+    void Start()
+    {
+        timer = Time.time;
+    }
+
+    void Update()
+    {
+        if (victim == null)
+            return;
+        if (Time.time - timer > speed)
+        {
+            new Damage(Actor(), victim, damage).Apply();
+            timer = Time.time;
+        }
+    }
+
+    public void Start(Player victim)
+    {
+        this.victim = victim;
+    }
+
+    public void Stop(Player escape)
+    {
+        if (escape.Equals(victim))
+            victim = null;
+    }
+}
