b682b9050d96466d36b0d1dfe8d7543f5a352791
Bohdan Vereshchak <veres.bs@gmail.com>
2019-12-19 20:42:37 +0100

    fix, added lost code

--- Assets/Game/Game.cs
+++ Assets/Game/Game.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Game
 {
+    public const int TILE_LAYER = 9;
+    public const int ACTOR_LAYER = 10;
+    public const int NODE_LAYER = 11;
+
+    public const string PLAYER_TAG = "P";
+
     private static Game instance;
 
     public static Game Instance
@@ -18,9 +26,26 @@ public class Game
 
     public World world;
 
+    public AudioCache audio;
+
     private Game()
     {
         world = GameObject.Find("World").GetComponent<World>();
+        audio = new AudioCache().Load();
+    }
+
+    public static void PlayerDeath()
+    {
+        if (EditorUtility.DisplayDialog("Game over", "You died!", "Exit", "Restart"))
+        {
+            Exit();
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+            Restart();
+        }
     }
 
     public static void Restart()
---
+++ Assets/Game/World/Actor/Mob/MobAwarenes.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobAwarenes : ActorBehaviour<Mob>
+{
+    public float fovRadius;
+    public Vector2 fovOffset;
+
+    private Player player;
+    private CircleCollider2D fieldOfView;
+
+    void Start()
+    {
+        fovRadius = 2;
+        fovOffset = new Vector2(0, 1);
+        fieldOfView = gameObject.AddComponent<CircleCollider2D>();
+        fieldOfView.radius = fovRadius;
+        fieldOfView.offset = fovOffset;
+        fieldOfView.isTrigger = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == Game.PLAYER_TAG)
+        {
+            player = collision.gameObject.GetComponent<Player>();
+        }
+    }
+
+    public void Target(Player player)
+    {
+        this.player = player;
+    }
+
+    public Player TargetInRange()
+    {
+        return player;
+    }
+}
